7299303a7ea468d8b6d598aba9e04095205b2d9b
unknown <pratiksaha136@gmail.com>
2023-09-01 14:42:24 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+export default function () =>{
+
+    return(
+        
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Journal Space</title>
+    </head>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="">
+    <style>
+       
+
+        
+    
+
+    );
+}
